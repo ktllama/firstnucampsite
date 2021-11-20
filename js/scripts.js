8358a5e529e0593650d579523944b0e4c04f6ee2
Katie <costantinkp@gmail.com>
2021-11-20 12:01:03 -0500

    changed css of headers for modals and added js to open modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 
-        $(function () {
-            $('[data-toggle="tooltip"]').tooltip()
-        })
-
+//        $(function () {
+//            $('[data-toggle="tooltip"]').tooltip()
+//        })
 
 $(function() {
     $(".carousel").carousel({interval:2000});
@@ -17,6 +16,20 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+
+
+
+
+
+
 });
 //this code takes the click of a button event and looks to see if the class is fa-pause if it is it pauses it, removed that class and adds a play button
 //to turn the play button back to pause there is a code below that reverses it with an else block
